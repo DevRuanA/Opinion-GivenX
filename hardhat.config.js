1fe938b047601d8cc5774dd36221cf383952a968
Ruan Almeida <ruanalmeida134@gmail.com>
2023-10-05 16:58:09 -0300

    Subindo os contratos inteligentes no app do Alchemy
    Criando um app na Alchemy
    Solicitando Ethereums de teste nas páginas de faucet
    Subindo os contratos inteligentes no app do Alchemy

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,3 +4,16 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.19",
 };
+
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    staging: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/XKGqX-ghxtd7GuskdAvkaDtS5BT_reL7	", //COLE O ENDEREÇO DO APP DENTRO DAS ASPAS
+      accounts: [""], //COLE SUA CHAVE PRIVADA DENTRO DAS ASPAS
+    },
+  }
+};
