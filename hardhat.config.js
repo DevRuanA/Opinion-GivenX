4039fee24f9706d1040c2b6e94c0cdb501634980
Ruan Almeida <ruanalmeida134@gmail.com>
2023-10-05 17:10:41 -0300

    Adicionando as funções de integração com o MetaMask
    integração com a metamask

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,7 +13,7 @@ module.exports = {
   networks: {
     staging: {
       url: "https://eth-sepolia.g.alchemy.com/v2/XKGqX-ghxtd7GuskdAvkaDtS5BT_reL7	", //COLE O ENDEREÇO DO APP DENTRO DAS ASPAS
-      accounts: [""], //COLE SUA CHAVE PRIVADA DENTRO DAS ASPAS
+      accounts: ["4e3d5debe79219e33882cc4d486d4a66971d5d0e3b19b1a2a13f5932dfe8a3f7"], //COLE SUA CHAVE PRIVADA DENTRO DAS ASPAS
     },
   }
 };
